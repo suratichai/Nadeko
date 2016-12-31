9eb2012263b2f58c970a585eec7bf63169d1d4e2
T44Nadeko <fearnlj01@track44.moe>
2016-12-31 16:26:56 +0000

    Weather fix and help string fix

--- src/NadekoBot/Modules/Searches/Commands/Models/WeatherModels.cs
+++ src/NadekoBot/Modules/Searches/Commands/Models/WeatherModels.cs
@@ -23,7 +23,7 @@ namespace NadekoBot.Modules.Searches.Commands.Models
     public class Main
     {
         public double temp { get; set; }
-        public int pressure { get; set; }
+        public double pressure { get; set; }
         public int humidity { get; set; }
         public double temp_min { get; set; }
         public double temp_max { get; set; }
