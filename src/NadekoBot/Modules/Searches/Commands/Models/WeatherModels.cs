29eedcd3b489222cb9c494f7f1365fbf96191939
T44Nadeko <fearnlj01@track44.moe>
2017-01-01 03:16:18 +0000

    muh \'freedom\'

--- src/NadekoBot/Modules/Searches/Commands/Models/WeatherModels.cs
+++ src/NadekoBot/Modules/Searches/Commands/Models/WeatherModels.cs
@@ -23,15 +23,39 @@ namespace NadekoBot.Modules.Searches.Commands.Models
     public class Main
     {
         public double temp { get; set; }
+        public string tempA {
+            get {
+                double tempA = (double)temp * 1.8 + 32;
+                return tempA.ToString("N1");
+            }
+        }
         public double pressure { get; set; }
         public int humidity { get; set; }
         public double temp_min { get; set; }
+        public string temp_minA {
+            get {
+                double temp_minA = (double)temp_min * 1.8 + 32;
+                return temp_minA.ToString("N1");
+            }
+        }
         public double temp_max { get; set; }
+        public string temp_maxA {
+            get {
+                double temp_maxA = (double)temp_max * 1.8 + 32;
+                return temp_maxA.ToString("N1");
+            }
+        }
     }
 
     public class Wind
     {
         public double speed { get; set; }
+        public string speedA {
+            get {
+                double speedA = (double)speed * 0.621371;
+                return speedA.ToString("N1");
+            }
+        }
         public double deg { get; set; }
     }
 
