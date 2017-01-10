3dafc55df24b0b55208a85b94408cfadde220d5f
T44Nadeko <fearnlj01@track44.moe>
2017-01-10 04:31:20 +0000

    🍍

--- src/NadekoBot/Services/Impl/StatsService.cs
+++ src/NadekoBot/Services/Impl/StatsService.cs
@@ -14,7 +14,7 @@ namespace NadekoBot.Services.Impl
         private ShardedDiscordClient client;
         private DateTime started;
 
-        public const string BotVersion = "1.1.0-beta";
+        public const string BotVersion = "1.1.0-🍍";
 
         public string Author => "Kwoth#2560";
         public string Library => "Discord.Net";
